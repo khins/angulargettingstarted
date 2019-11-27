753ec6359066e5b5a6da6e60f78a3326c88563bd
khins <kevin@hinshawfam.com>
2019-11-27 08:00:51 -0600

    updated

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent{
  pageTitle: string = 'Product Extravaganze List!!';
-/*  products: any[] =[
+ products: any[] =[
     {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -26,6 +26,36 @@ export class ProductListComponent{
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "assets/images/garden_cart.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2019",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "assets/images/hammer.png"
+      },
+      {
+        "productId": 8,
+        "productName": "Saw",
+        "productCode": "TBX-0022",
+        "releaseDate": "May 15, 2019",
+        "description": "15-inch steel blade hand saw",
+        "price": 11.55,
+        "starRating": 3.7,
+        "imageUrl": "assets/images/saw.png"
+      },
+      {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2018",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "assets/images/xbox-controller.png"
       }
- ]; */
+ ];
 }
