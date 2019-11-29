b0a42ca1968401eaac99a69a9571a8a4ed7329bf
khins <kevin@hinshawfam.com>
2019-11-29 06:09:08 -0600

    adds filtered by

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -9,6 +9,7 @@ export class ProductListComponent{
  imageWidth: number=50;
  imageMargin: number=2;
  showImage: boolean = false;
+ listFiler: string='cart';
  products: any[] =[
     {
         "productId": 1,
