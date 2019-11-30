923f3373e269ca30c61d99cb88df8c0490ecf85f
khins <kevin@hinshawfam.com>
2019-11-30 08:25:35 -0600

    adds oninit lifecycle hook

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,10 @@ import { IProduct } from './product';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
   })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('in OnInit');
+  }
  pageTitle: string = 'Product Extravaganza List!!';
  imageWidth: number=50;
  imageMargin: number=2;
